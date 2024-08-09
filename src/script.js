dc315e847516f0cf7a3d95533f41bdd5203399bd
hellodayy <ohlemon0678@gmail.com>
2024-08-09 15:30:33 +0700

    add AI API and javascript

---
+++ src/script.js
@@ -0,0 +1,32 @@
+function displayQuotes(response) {
+  new Typewriter("#generating-quote", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+function GetApi(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#input-topic");
+  let apiKey = "0a22ffd903a7dat6od52d64479b35f32";
+  let prompt =
+    "You are a generator quotes expert and love to write short quotes. You mission is to generate a quotes in 2 or 3 sentences, and use <blockquote> around the quote and at the end. Make sure to follow the user instructions.";
+  let context = `User instructions: Generate a quotes about ${userInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayQuotes);
+
+  let quotesContainer = document.querySelector("#quotes-container");
+  quotesContainer.classList.add("hidden");
+  let generatingQuote = document.querySelector("#generating-quote");
+  new Typewriter("#generating-quote", {
+    strings: `generate quotes for topic ${userInput.value}`,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let inputForm = document.querySelector("#submit-button");
+inputForm.addEventListener("click", GetApi);
